8c97b14114c99084d0cd3585e10cc9b59db27114
GustavoMarques06 <161479140+GustavoMarques06@users.noreply.github.com>
2025-05-15 22:11:05 -0300

    corrigir erro de Js e Css

--- admPage.php
+++ admPage.php
@@ -1,20 +1,22 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ADM page</title>
-    <link rel="stylesheet" href="src/style/styleADM.css">.
+    <link rel="stylesheet" href="styles/styleADM.css">.
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 
 </head>
+
 <body>
     <img src="src/logotray.png" class="logo" alt="Logo">
     <nav class="menu-lateral">
         <div class="btn-expandir">
             <i class="bi bi-list" id="bnt-exp"></i>
         </div>
-        <script src="src/javascript/expandirMenuADM.js"></script>
+        <script src="js/expandirMenuADM.js"></script>
         <ul>
             <li class="item-menu">
                 <a href="#" id="homeLink">
@@ -47,6 +49,7 @@
         Gerenciar os projetos vinculados a cada colaborador.
     </div>
 
-    <script src="src/javascript/menuADM.js"></script>
+    <script src="js/menuADM.js"></script>
 </body>
+
 </html>
