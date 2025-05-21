38630ac0e70f3b733e80ee11f0331f1f4782c0c2
Nicolas_Tonsa <nicoprogramas1234@gmail.com>
2025-05-21 18:07:14 -0300

    implementação da listagem na admPage

--- js/menuADM.js
+++ js/menuADM.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 // Ativa destaque visual do menu
 const menuItem = document.querySelectorAll('.item-menu');
 menuItem.forEach((item) =>
@@ -36,4 +37,35 @@ document.getElementById('funcionarioLink').addEventListener('click', function (e
   e.preventDefault();
   esconderTodosConteudos();
   funcionarioContent.style.display = 'block';
+=======
+var menuItem = document.querySelectorAll('.item-menu');
+
+function selecionarLink(){
+    menuItem.forEach((item) =>
+        item.classList.remove('ativos'));
+    this.classList.add('ativos');
+}
+
+menuItem.forEach((item) => item.addEventListener('click', selecionarLink));
+
+document.getElementById('homeLink').addEventListener('click', function(event) {
+    event.preventDefault();
+    document.getElementById('homeContent').style.display = 'block';
+    document.getElementById('funcionarioContent').style.display = 'none';
+    document.getElementById('clienteContent').style.display = 'none';
+});
+
+document.getElementById('funcionarioLink').addEventListener('click', function(event) {
+    event.preventDefault();
+    document.getElementById('homeContent').style.display = 'none';
+    document.getElementById('clienteContent').style.display = 'none';
+    document.getElementById('funcionarioContent').style.display = 'block';
+});
+
+document.getElementById('clienteLink').addEventListener('click', function(event) {
+    event.preventDefault();
+    document.getElementById('homeContent').style.display = 'none';
+    document.getElementById('funcionarioContent').style.display = 'none';
+    document.getElementById('clienteContent').style.display = 'block';
+>>>>>>> Stashed changes
 });
